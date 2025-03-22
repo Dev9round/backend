bdfc98359923530d48b918e694bfa2cfbb95a289
ksundev <kshk08@hanyang.ac.kr>
2025-03-22 22:25:15 +0900

    Initialize Spring Boot project with Gradle wrapper and basic configuration

---
+++ src/main/java/com/ksundev/tallyhoserver/TallyhoServerApplication.java
@@ -0,0 +1,13 @@
+package com.ksundev.tallyhoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallyhoServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TallyhoServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ksundev/tallyhoserver/TallyhoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksundev.tallyhoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallyhoServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
